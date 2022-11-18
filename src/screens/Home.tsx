bddf379b424cea48b191ef19cfa8d074d41efcaa
AndreCosta <andre.costa101@gmail.com>
2022-11-18 17:58:23 -0300

    feat(Home): list exercises

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,39 @@
-import { VStack} from 'native-base';
+import { useState } from 'react';
+import { VStack, HStack, FlatList} from 'native-base';
+import { Group } from '../components/Group';
 import { HomeHeader } from '../components/HomeHeader';
 
 export function Home(){
+    const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Peito', 'Pernas', 'Ombros']);
+    const [ groupSelected, setGroupSelected ] = useState('Costas');
+
+
     return (
         <VStack flex={1}>
             <HomeHeader/>
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px: 8}}  
+                my={10}
+                maxH={10}
+                renderItem={({item}) => (
+                    <Group 
+                        name={item}
+                        isActive={groupSelected === item} 
+                        onPress={() => setGroupSelected(item)}
+                    />
+                )}
+
+            />
+            <HStack>
+
+
+            </HStack>
+
         </VStack>
     )
 }

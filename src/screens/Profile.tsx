e80b78e9b0b55058cbb96e96a4b1b95c27366fd6
AndreCosta <andre.costa101@gmail.com>
2022-11-22 15:38:26 -0300

    feat(Profile): form

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,8 +1,10 @@
-import { VStack, ScrollView, Center, Skeleton, Text} from 'native-base';
+import { VStack, ScrollView, Center, Skeleton, Text, Heading} from 'native-base';
 import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { ScreenHeader } from '../components/ScreenHeader';
 import { UserPhoto } from '../components/UserPhoto';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 
 const PHOTO_SIZE = 33;
 
@@ -33,8 +35,46 @@ export function Profile(){
                             Alterar foto
                         </Text>
                     </TouchableOpacity>
+
+                    <Input
+                        bg='gray.600'
+                        placeholder='Nome'
+                    />
+
+                    <Input
+                        bg='gray.600'
+                        placeholder='E-mail'
+                        isDisabled
+                    />
+
+                    <Heading color='gray.200' fontSize="md" mb={2} alignSelf='flex-start' mt={12}>
+                        Alterar Senha
+                    </Heading>
+                    
+                    <Input 
+                        bg='gray.600'
+                        placeholder='Senha antiga'
+                        secureTextEntry
+                    />
+
+                    <Input 
+                        bg='gray.600'
+                        placeholder='Nova senha'
+                        secureTextEntry
+                    />
+
+                    <Input 
+                        bg='gray.600'
+                        placeholder='Confirme nova senha'
+                        secureTextEntry
+                    />
+
+                    <Button
+                        title='Atualizar'
+                        mt={4}
+
+                    />
                 </Center>
-                
             </ScrollView>
         </VStack>
     )

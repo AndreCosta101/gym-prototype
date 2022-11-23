f1485b886c4c793d8d40d7ba669cc3a069a93516
AndreCosta <andre.costa101@gmail.com>
2022-11-23 14:56:33 -0300

    feat(Exercise): body

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,12 @@
-import { VStack, Text, Icon, HStack, Heading} from 'native-base';
+import { VStack, Text, Icon, HStack, Heading, Image, Box} from 'native-base';
 import { TouchableOpacity } from 'react-native';
 import {Feather} from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { AppNavigatorRoutesProps } from '../routes/app.routes';
 import BodySvg from '../assets/body.svg';
+import SeriesSvg from '../assets/series.svg';
+import RepetitionsSvg from '../assets/repetitions.svg';
+import { Button } from '../components/Button';
 
 export function Exercise(){
     const navigation = useNavigation<AppNavigatorRoutesProps>();
@@ -14,6 +17,7 @@ export function Exercise(){
 
     return (
         <VStack flex={1}>
+            {/* Header */}
             <VStack px={8} bg='gray.600' pt={12}>
                 <TouchableOpacity onPress={handleGoBack}>
                     <Icon as={Feather} name='arrow-left' color='green.500' size={6} />
@@ -32,6 +36,43 @@ export function Exercise(){
                         </Text>
                     </HStack>
                 </HStack>
+            </VStack>
+
+            {/* Body */}
+
+            <VStack p={8}>
+                <Image
+                    w='full'
+                    h={80}
+                    source={{ uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg' }}
+                    alt='Nome do exercício'
+                    mb={3}
+                    resizeMode='cover'
+                    rounded='lg'
+                />
+
+                <Box bg='gray.600' rounded='md' pb={4} px={4}>   
+                    <HStack alignItems='center' justifyContent='space-around' mb={6} mt={5}>
+                        <HStack>
+                            <SeriesSvg/>
+                            <Text color='gray.200' ml={2} textTransform='capitalize'>
+                                3 séries
+                            </Text>
+                        </HStack>
+                        <HStack>
+                            <RepetitionsSvg/>
+                            <Text color='gray.200' ml={2} textTransform='capitalize'>
+                                12 repetições
+                            </Text>
+                        </HStack>
+                    </HStack>  
+
+                    <Button 
+                        title='Marcar como realizado' 
+                        onPress={() => {}}
+                    />           
+
+                </Box>
             </VStack>
         </VStack>
     )
